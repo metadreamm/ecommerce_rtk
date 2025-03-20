3162409fc514ed1cd1e7757e266ca1ffcd379fb9
metadreamm <soxegor@gmail.com>
2025-03-20 17:34:27 -0400

    practice task

---
+++ src/Components/SuperCoin.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useState } from 'react'
+import { useSelector } from 'react-redux';
+
+const SuperCoin = () => {
+    const [SuperCoin, setSuperCoins] = useState(0);
+
+    return (
+        <>
+
+        </>
+    )
+}
